a443bff05b7ab1aaedcb7c903daf8c8ec59a934a
Gustavofurtad2 <gustav0furt.fatality@gmail.com>
2025-03-12 19:49:46 -0300

    refactor

--- inc/scenes/gameplay/gameplay.hpp
+++ inc/scenes/gameplay/gameplay.hpp
@@ -35,7 +35,7 @@ class Gameplay : public Cs::Scene {
 
         Cs::SceneManager& sceneManager;
 
-        Image loadingBackground;
-        LoadingScreen loadingScreen;
+        Image* loadingBackground;
+        LoadingScreen* loadingScreen;
 
 };
--- src/scenes/gameplay/gameplay.cpp
+++ src/scenes/gameplay/gameplay.cpp
@@ -3,9 +3,10 @@
 #include "scenes/gameplay/gameplay.hpp"
 #include "game.hpp"
 
-Gameplay::Gameplay(Cs::SceneManager& _sceneManager)
+Gameplay::Gameplay(Cs::SceneManager& _sceneManager) 
   : sceneManager(_sceneManager),
-    loadingScreen(5) {}
+    loadingScreen(new LoadingScreen(5)),
+    loadingBackground(new Image()) {}
 
 Gameplay::~Gameplay() {
 
@@ -16,55 +17,58 @@ void Gameplay::init() {
 
     auto& renderer = Cs::GetEngine()->renderer;
 
-    loadingBackground.init("assets/menu/background.png", Tyra::Vec2(0, 0), Tyra::Vec2(512, 448));
+    loadingBackground->init("assets/menu/background.png", Tyra::Vec2(0, 0), Tyra::Vec2(512, 448));
 
-    loadingScreen.init();
+    loadingScreen->init();
 
     renderer.beginFrame();
-    loadingBackground.render();
-    loadingScreen.handleLoader();
+    loadingBackground->render();
+    loadingScreen->handleLoader();
     renderer.endFrame();
 
-    auto hud = loadingScreen.addTask<HUD>();
+    auto hud = loadingScreen->addTask<HUD>();
 
     hud->init();
 
     renderer.beginFrame();
-    loadingBackground.render();
-    loadingScreen.handleLoader();
+    loadingBackground->render();
+    loadingScreen->handleLoader();
     renderer.endFrame();
 
-    auto usp = loadingScreen.addTask<Gun>();
+    auto usp = loadingScreen->addTask<Gun>();
 
     renderer.beginFrame();
-    loadingBackground.render();
-    loadingScreen.handleLoader();
+    loadingBackground->render();
+    loadingScreen->handleLoader();
     renderer.endFrame();
 
-    auto ak47 = loadingScreen.addTask<Gun>();
+    auto ak47 = loadingScreen->addTask<Gun>();
 
     renderer.beginFrame();
-    loadingBackground.render();
-    loadingScreen.handleLoader();
+    loadingBackground->render();
+    loadingScreen->handleLoader();
     renderer.endFrame();
 
-    player = loadingScreen.addTask<Player>(std::move(hud), std::move(usp), std::move(ak47));
+    player = loadingScreen->addTask<Player>(std::move(hud), std::move(usp), std::move(ak47));
     
     player->init();
 
     renderer.beginFrame();
-    loadingBackground.render();
-    loadingScreen.handleLoader();
+    loadingBackground->render();
+    loadingScreen->handleLoader();
     renderer.endFrame();
 
-    map = loadingScreen.addTask<Model>();
+    map = loadingScreen->addTask<Model>();
 
     map->init("assets/gameplay/maps/de_dust2/De_dust2.obj", "assets/gameplay/maps/de_dust2/", 500.0f);
 
     renderer.beginFrame();
-    loadingBackground.render();
-    loadingScreen.handleLoader();
+    loadingBackground->render();
+    loadingScreen->handleLoader();
     renderer.endFrame();
+
+    delete loadingScreen;
+    delete loadingBackground;
 
     Cs::GetEngine()->renderer.setClearScreenColor(Tyra::Color(122, 202, 255));
 }
